fe93c8404fb1c98b2b5e0413e3c238dd200223f5
mkhodroo <mkhodroo@yahoo.com>
2023-02-10 23:46:12 +0330

    Update store.blade.php

--- resources/views/js/store.blade.php
+++ resources/views/js/store.blade.php
@@ -1,13 +1,16 @@
 <script>
-    function show_catagory_by_part_of_name(name){
+    function show_catagory_by_part_of_name(name, element = "main-content"){
         var url = "{{ route('show-catagory-by-part-of-name', ['name' => 'cat_name']) }}"
         url = url.replace("cat_name", name);
         send_ajax_get_request(
             url,
             function(body){
-                $('#main-content').html(body)
+                $(`#${element}`).html(body)
             }
         )
+        $(`html, body`).animate({
+            scrollTop: $(`#${element}`).offset().top
+        }, 1000);
     }
 
     $(".select2").select2();
