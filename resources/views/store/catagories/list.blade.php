0da1207e5d4c86e4a285e7cd91e8fe0d5c9b9619
mkhodroo <mkhodroo@yahoo.com>
2023-02-11 00:53:18 +0330

    Update list.blade.php

--- resources/views/store/catagories/list.blade.php
+++ resources/views/store/catagories/list.blade.php
@@ -14,7 +14,7 @@
                 ])
             </td>
             <td>{{ $product->name }}</td>
-            <td class="camma-value">{{ $product->min_price()->price ?? '' }}</td>
+            <td><span class="camma-value">{{ $product->min_price()->price ?? '' }}</span> ت </td>
             <td width="150">
                 <div class="buttons">
                     <a class="btn btn-theme btn-theme-transparent btn-icon-left" style="background: #db3537; width: 100%; border-radius: 10px" onclick="add_to_cart({{ $product->producer()->id }})"><i class="fa fa-shopping-cart"></i>افزودن به سبد</a>
