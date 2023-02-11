5e99426743338ff90eb0fe7e594b7886d2101eac
mkhodroo <mkhodroo@yahoo.com>
2023-02-11 17:58:00 +0330

    Update contact-us.blade.php

--- resources/views/store/home/contact-us.blade.php
+++ resources/views/store/home/contact-us.blade.php
@@ -13,7 +13,15 @@
         <div class="container full-width text-center">
             <div class="col-sm-4" style="padding: 10px; background: red; color: white; margin: 10px; border-radius: 10px">
                 شماره پیامکی جهت ارسال پیام: 
-                300045054505
+                <a href="sms:30007732008850" style="color: blue">30007732008850</a>
+            </div>
+            <div class="col-sm-4" style="padding: 10px; background: rgb(255, 157, 0); color: white; margin: 10px; border-radius: 10px">
+                شماره تماس:  
+                <a href="tel:02191307268" style="color: blue">02191307268</a>
+            </div>
+            <div class="col-sm-4" style="padding: 10px; background: rgb(255, 157, 0); color: white; margin: 10px; border-radius: 10px">
+                آدرس داروخانه مرکزی: 
+                تهران- شهرستان بهارستان - شهر گلستان - میدان سعدی - خیابان نسترن - میدان فاطمیه - پلاک 109 دارخانه دکتر احسانی کیا
             </div>
         </div>
     </section>
