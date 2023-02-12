4678947931a8f10c16fd1ce5d2fd54a95a050446
mkhodroo <mkhodroo@yahoo.com>
2023-02-12 18:56:23 +0330

    Update get.blade.php

--- resources/views/admin/articles/get.blade.php
+++ resources/views/admin/articles/get.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('content')
+<div class="box">
+    <button class="btn btn-danger" onclick="get_from_darukade()">{{ __('get from darukade') }}</button>
+</div>
     <table class="table table-striped" id="article-table">
 
     </table>
@@ -14,30 +17,35 @@
     <form action="" id="article-form">
         @csrf
         <input type="text" name="title" id="title">
+        <input type="text" name="excerpt" id="excerpt">
         <input type="text" name="content" id="content">
     </form>
     <script>
         var art_table = $('#article-table');
-        send_ajax_get_request(
-            'https://www.darukade.com/mag/articles',
-            function(data){
-                $('#demo').html(data)
-                var articles = $('article')
-                // console.log(articles);
-                articles.each(function(item){
-                    var art_thumb = $(this).find('.img-layer img').attr("src");
-                    var art_title = $(this).find('.left-side h2').html();
-                    var art_link = $(this).find('.left-side h2 a').attr("href");
-                    art_table.append(`<tr>`)
-                    art_table.append(`<td> <img src="https://www.darukade.com/${art_thumb}" width="100"> </td>`)
-                    art_table.append(`<td> ${art_title} </td>`)
-                    art_table.append(`<td> ${art_link} </td>`)
-                    art_table.append(`<td> <button onclick="get_article('${art_link}')">get</button> </td>`)
-                    art_table.append(`</tr>`)
-                    
-                })
-            }
-        )
+        function get_from_darukade(){
+            send_ajax_get_request(
+                'https://www.darukade.com/mag/articles',
+                function(data){
+                    $('#demo').html(data)
+                    var articles = $('article')
+                    // console.log(articles);
+                    articles.each(function(item){
+                        var art_thumb = $(this).find('.img-layer img').attr("src");
+                        var art_title = $(this).find('.left-side h2').html();
+                        var art_link = $(this).find('.left-side h2 a').attr("href");
+                        art_table.append(`<tr>`)
+                        art_table.append(`<td> <img src="https://www.darukade.com/${art_thumb}" width="100"> </td>`)
+                        art_table.append(`<td> ${art_title} </td>`)
+                        art_table.append(`<td> ${art_link} </td>`)
+                        art_table.append(`<td> <button onclick="get_article('${art_link}')">get</button> </td>`)
+                        art_table.append(`</tr>`)
+                        
+                    })
+                    $('#demo').html('')
+                }
+            )
+        }
+        
 
         function get_article(link){
             link = `https://www.darukade.com/${link}`;
@@ -46,8 +54,10 @@
                 function(data){
                     $('#demo').html(data)
                     var title = $('header h1 a').html();
+                    var excerpt = $($('article p')[0]).html();
                     var article = $('article')
                     $('#article-form #title').val(title)
+                    $('#article-form #excerpt').val(title)
                     $('#article-form #content').val(article.html())
                     $('#demo').html('')
                     var fd = $('#article-form').serialize()
